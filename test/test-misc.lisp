b7d0f4de0c03df5c6b58bfcc7406204eaa15b546
Santanu Chakrabarti <santanuchakrabarti@users.noreply.github.com>
2022-02-16 18:44:57 +0530

    Complete test cases for current miscellaneous utilities w-i-p.

--- test/test-misc.lisp
+++ test/test-misc.lisp
@@ -2,4 +2,27 @@
 
 (clunit:deftest test-fibo (misc-tests)
   (clunit:assert-equalp (mu:create-seq #'mu:nxt-fibonacci '() 1) '(1))
-  (clunit:assert-equalp (mu:create-seq #'mu:nxt-fibonacci '() 2) '(1 1)))
+  (clunit:assert-equalp (mu:create-seq #'mu:nxt-fibonacci '() 2) '(1 1))
+  (clunit:assert-equalp (mu:create-seq #'mu:nxt-fibonacci '() 3) '(1 1 2))
+  (clunit:assert-equalp (mu:create-seq #'mu:nxt-fibonacci '() 5) '(1 1 2 3 5)))
+
+(clunit:deftest test-power-of2 (misc-tests)
+  (clunit:assert-equalp (mu:create-seq #'mu:nxt-power-of2 '() 1) '(2))
+  (clunit:assert-equalp (mu:create-seq #'mu:nxt-power-of2 '() 2) '(2 4))
+  (clunit:assert-equalp (mu:create-seq #'mu:nxt-power-of2 '() 3) '(2 4 8)))
+
+(clunit:deftest test-in-range-of (misc-tests)
+  (clunit:assert-true (mu:in-range-of 1.80 1.8034))
+  (clunit:assert-true (mu:in-range-of 0.00 0 :range 0.0001))
+  (clunit:assert-false (mu:in-range-of 1.80 2.0))
+  (clunit:assert-false (mu:in-range-of 1.80 1.8034 :range 0.0001)))
+
+(clunit:deftest test-^ (misc-tests)
+  (clunit:assert-equalp (mu:^ 2 3) 8)
+  (clunit:assert-equalp (mu:^ 2 -3) 1/8)
+  (clunit:assert-equalp (mu:^ 2 2.7) 8)
+  (clunit:assert-equalp (mu:^ 2 2.2) 4)
+  (clunit:assert-equalp (mu:^ 2.5 2) (expt 2.5 2)))
+  
+
+
--- test/test-suits.lisp
+++ test/test-suits.lisp
@@ -1,6 +1,7 @@
 (in-package :cl-myutils/test)
 
 (setf *print-pretty* t)
+(setf clunit:*clunit-report-format* :default)
 
 (clunit:defsuite all-tests ())
 (clunit:defsuite misc-tests (all-tests))
